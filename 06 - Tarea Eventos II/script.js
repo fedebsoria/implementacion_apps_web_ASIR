96d815f090c91bc7228741fd707840343229cebb
Federico B. Soria <fsoria7@outlook.com>
2025-12-15 14:21:55 +0100

    feat: start logic in script.js chore: change img01 name to img00 as it's the default image

---
+++ 06 - Tarea Eventos II/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttonCheckTemp = document.getElementById("check-temp-button");
+    let resultMessages = ["Temperatura baja. Encendiendo calefacción.", 
+        "Temperatura ideal. Sistema estable.", 
+        "Temperatura alta. Activando ventilación.",
+        "Alerta: temperatura excesiva. Activando enfriamiento máximo."];
+    let resultImg = ["img01", "img02", "img03","img04"];
+    const temperatureUser = document.getElementById()
+
+});
