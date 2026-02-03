d9d31f8d7b26f7d0df99ea74965ab47190c5da4b
fedebsoria <fsoria7@outlook.com>
2026-02-03 11:34:37 +0100

    feature: add images to the DOM with JS. Give style to them with css

--- 10 - Práctica visor II/script.js
+++ 10 - Práctica visor II/script.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const visor = document.getElementById('image-visor');
-
+    
     const images = [
-        'img/foto1.jpg', 
-        'img/foto2.jpg', 
-        'img/foto3.jpg', 
-        'img/foto4.jpg', 
-        'img/foto5.jpg', 
-        'img/foto6.jpg'
+        'img/foto1.jpg', 'img/foto2.jpg', 
+        'img/foto3.jpg', 'img/foto4.jpg', 
+        'img/foto5.jpg', 'img/foto6.jpg'
     ];
+
+    let actualIndex = 0;
 
     const imagesText = [
         'El pueblo amanece nevado.',
@@ -23,6 +22,34 @@ document.addEventListener('DOMContentLoaded', () => {
         'img/dry-clean.png', 
         'img/new-moon.png', 
         'img/flecha-correcta-izq.png', 
-        'img/flecha-correcta-izq.png'
+        'img/flecha-correcta-der.png'
     ];
+
+    const img = document.createElement('img');
+    img.src = images[actualIndex];
+    img.className = 'miniature';
+
+    const arrowL = document.createElement('img');
+    arrowL.className = 'arrow';
+    arrowL.id = 'arrowL';
+    arrowL.src = visorParts[2];
+
+    const arrowR = document.createElement('img');
+    arrowR.className = 'arrow';
+    arrowR.id = 'arrowR';
+    arrowR.src = visorParts[3];
+
+    const dotEmpty = document.createElement('img');
+    dotEmpty.className = 'dotEmpty';
+    dotEmpty.src =visorParts[0];
+
+    const dotFull = document.createElement('img');
+    dotFull.className = 'dotFull';
+    dotFull.id = 'dotFull';
+    dotFull.src = visorParts[1];
+
+    visor.appendChild(arrowL);
+    visor.appendChild(img);
+    visor.appendChild(arrowR);
+
 });
