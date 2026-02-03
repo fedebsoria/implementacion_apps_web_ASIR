bd6a4a6b685e27ec41936a780ca77ee6ae65b67b
fedebsoria <fsoria7@outlook.com>
2026-02-03 10:48:31 +0100

    feature: create index div for the image visor, create variables for JS element's sources

---
+++ 10 - Práctica visor II/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const visor = document.getElementById('image-visor');
+
+    const images = [
+        'img/foto1.jpg', 
+        'img/foto2.jpg', 
+        'img/foto3.jpg', 
+        'img/foto4.jpg', 
+        'img/foto5.jpg', 
+        'img/foto6.jpg'
+    ];
+
+    const imagesText = [
+        'El pueblo amanece nevado.',
+        'La nieve cubre todo.',
+        'El lago se congela y los pinos se visten de blanco',
+        'Nos preparamos para la aventura',
+        'Los picos más altos nos saludan con blanco esplendor',
+        'El frío no nos detiene'
+    ];
+
+        const visorParts = [
+        'img/dry-clean.png', 
+        'img/new-moon.png', 
+        'img/flecha-correcta-izq.png', 
+        'img/flecha-correcta-izq.png'
+    ];
+});
