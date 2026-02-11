71bd0d8cc77e7592cd461f63fe326a17f2c34431
fedebsoria <fsoria7@outlook.com>
2026-02-11 15:46:24 +0100

    feature: create new class that creates an object with information about a bank account. Has a method for printing the info of the account and another method to do withdrawls that checks if the account has enough money, if not prints a message and doesn't do the operation

--- 13 - Práctica Trabajjo con Clases/script.js
+++ 13 - Práctica Trabajjo con Clases/script.js
@@ -11,3 +11,47 @@
 opción al usuario.
 4. Crea tres objetos basados en esta clase. */
 
+class cuentaBancaria {
+    // 1 propiedades de la clase
+    titular;
+    iban;
+    saldo;
+    activa;
+
+    constructor (titular, iban, saldo, activa){
+        this.titular = titular;
+        this.iban = iban;
+        this.saldo = saldo;
+        this.activa = activa;
+    }
+
+    printInfo () {
+        console.log(`titular: ${this.titular}\niban: ${this.iban}\nsaldo: ${this.saldo}€\nactiva: ${this.activa}`);
+    }
+
+    retiro (monto) {
+        if (monto > this.saldo) {
+            console.log(`El monto ${monto} a retirar es mayor al saldo ${this.saldo}`);
+        } else {
+            this.saldo -= monto;
+            console.log(`${monto}€ retirados. Saldo actual:${this.saldo}€`)
+        }
+    }
+}
+
+//Primer nuevo obejeto y prueba de métodos.
+const cuentaPaco = new cuentaBancaria("Paco De Lucia", "ES000001", 1000, true);
+
+cuentaPaco.printInfo();
+cuentaPaco.retiro(101);
+
+//Segundo objeto
+const cuentaPaquita = new cuentaBancaria("Paquita La Del Barrio", "ES000002", 19999, false);
+cuentaPaquita.printInfo();
+cuentaPaquita.retiro(1999999);
+
+//Tercer Objeto
+const cuentaCamaron = new cuentaBancaria("Camarón De La Isla", "ES000003", 7777,  true);
+cuentaCamaron.printInfo();
+cuentaCamaron.retiro(7);
+cuentaCamaron.retiro(7777);
