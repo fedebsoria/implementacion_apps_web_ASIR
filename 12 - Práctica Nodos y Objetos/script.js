b52735a5354084cf04f4bad79d966507ba28e85b
fedebsoria <fsoria7@outlook.com>
2026-02-11 10:36:56 +0100

    feat: create objects in script.js that will be used as content for the web site

---
+++ 12 - Práctica Nodos y Objetos/script.js
@@ -0,0 +1,27 @@
+document.addEventListener( 'DOMContentLoaded', () => {
+    const parent = document.getElementById('parent');
+
+    const alumna1 = {
+        imagen : 'img/alumna1.jpg',
+        nombre : 'Cristina',
+        apellido : 'Lopez',
+        edad : '24',
+        curso : '2º DAM'
+    }
+
+    const alumno2 = {
+        img : 'img/alumno2.png',
+        nombre : 'Manuel',
+        apellido : 'Guzmán',
+        edad : '18',
+        curso : '1º DAM'
+    }
+
+    const alumno3 = {
+        img : 'img/alumna3.jpg',
+        apellido : 'Ariza',
+        edad : '26',
+        curso : '2º DAW'
+    }
+
+});
