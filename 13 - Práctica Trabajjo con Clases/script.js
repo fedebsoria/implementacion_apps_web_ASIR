8facc4b7b50fc913630efc884c1cff2cce39237a
fedebsoria <fsoria7@outlook.com>
2026-02-11 15:13:21 +0100

    chore: create script.js file

---
+++ 13 - Práctica Trabajjo con Clases/script.js
@@ -0,0 +1,13 @@
+/* Práctica trabajo con Clases
+1.Crea la clase cuentaBancaria con las siguientes propiedades:
+• Titular
+• Iban
+• Saldo
+• Activa
+2.Crea el constructor para construir el objeto.
+3.Agrega los siguientes métodos:
+• Mostrar toda la información de las clases
+• Crea un método que si el cliente intenta retirar un importe superior al saldo, no le de
+opción al usuario.
+4. Crea tres objetos basados en esta clase. */
+
